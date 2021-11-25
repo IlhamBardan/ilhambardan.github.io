c715d7208abec00f04310b223b5fc38d4e797825
ilhambardan <p.kont60@gmail.com>
2021-11-25 22:14:32 +0700

    98% complete

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,43 +1,34 @@
 // Testimonial
 (() => {
-  const sliderContainer = document.querySelector(".testimonial-slider-container"),
-    slides = sliderContainer.querySelectorAll(".testimonial-item");
-  slideWidth = sliderContainer.offsetWidth;
-  (prevBtn = document.querySelector(".testimonial-slider-nav .prev")), (nextBtn = document.querySelector(".testimonial-slider-nav .next"));
-  activeSlider = sliderContainer.querySelector(".testimonial-item.active");
-  let slideIndex = Array.from(activeSlider.parentElement.children).indexOf(activeSlider);
+  var btnIndicator = document.getElementsByClassName("btn-indicator");
+  var testimonialSlide = document.getElementById("testimonial-slide");
 
-  //set width of all slides
-  slides.forEach((slide) => {
-    slide.style.width = slideWidth + "px";
-  });
-
-  //set width of sliderContainer
-  sliderContainer.style.width = slideWidth * slides.length + "px";
-  nextBtn.addEventListener("click", () => {
-    if (slideIndex === slides.length - 1) {
-      slideIndex = 0;
-    } else {
-      slideIndex++;
+  btnIndicator[0].onclick = function () {
+    testimonialSlide.style.transform = "translate(0px)";
+    for (i = 0; i < 4; i++) {
+      btnIndicator[i].classList.remove("active");
     }
-    slider();
-  });
-
-  prevBtn.addEventListener("click", () => {
-    if (slideIndex === 0) {
-      slideIndex = slides.length - 1;
-    } else {
-      slideIndex--;
+    this.classList.add("active");
+  };
+  btnIndicator[1].onclick = function () {
+    testimonialSlide.style.transform = "translate(-800px)";
+    for (i = 0; i < 4; i++) {
+      btnIndicator[i].classList.remove("active");
     }
-    slider();
-  });
-
-  function slider() {
-    // deactivate existing slider active
-    sliderContainer.querySelector(".testimonial-item.active").classList.remove("active");
-    // active new Slider
-    slides[slideIndex].classList.add("active");
-    sliderContainer.style.marginLeft = -(slideWidth * slideIndex) + "px";
-  }
-  slider();
+    this.classList.add("active");
+  };
+  btnIndicator[2].onclick = function () {
+    testimonialSlide.style.transform = "translate(-1600px)";
+    for (i = 0; i < 4; i++) {
+      btnIndicator[i].classList.remove("active");
+    }
+    this.classList.add("active");
+  };
+  btnIndicator[3].onclick = function () {
+    testimonialSlide.style.transform = "translate(-2400px)";
+    for (i = 0; i < 4; i++) {
+      btnIndicator[i].classList.remove("active");
+    }
+    this.classList.add("active");
+  };
 })();
