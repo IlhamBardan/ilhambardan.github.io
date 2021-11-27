09000aec11fb113eb6bcfbe641365668bda7d0f3
ilhambardan <p.kont60@gmail.com>
2021-11-27 15:31:13 +0700

    client map

--- assets/js/style.js
+++ assets/js/style.js
@@ -53,3 +53,37 @@
     }
   });
 })();
+
+// Google Map API
+function initMap() {
+  // Customizing Map
+  map = new google.maps.Map(document.getElementById("map"), {
+    mapId: "a080310845d9d8dd",
+    center: { lat: 48.85, lng: 2.35 },
+    zoom: 1.8,
+  });
+  // Marker Map
+  // Arab Saudi
+  marker = new google.maps.Marker({
+    position: { lat: 23.75479868008521, lng: 44.78677127204856 },
+    map,
+  });
+
+  // Belanda
+  marker = new google.maps.Marker({
+    position: { lat: 52.01627826286596, lng: 5.541515560606857 },
+    map,
+  });
+
+  // Perancis
+  marker = new google.maps.Marker({
+    position: { lat: 46.62585501478423, lng: 2.738491429027504 },
+    map,
+  });
+
+  // Jepang
+  marker = new google.maps.Marker({
+    position: { lat: 36.802929229421075, lng: 139.22376923994202 },
+    map,
+  });
+}
